27d7f2c1c152ba17339158a70f894773321d8a2d
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-11-02 21:24:16 -0300

    :sparkles: feature(components/atoms/icon-button): add optional tooltip prop

--- packages/components/src/atoms/icon-button/index.tsx
+++ packages/components/src/atoms/icon-button/index.tsx
@@ -1,19 +1,31 @@
 import type * as Stitches from '@stitches/react'
 
+import { Tooltip } from '@/atoms'
 import * as S from '@/atoms/button/style'
 
-type Props = {
+export type Props = {
   icon?: React.ReactNode
   css?: Stitches.CSS
   'aria-label': string
+  tooltip?: React.ReactNode
 } & React.ButtonHTMLAttributes<HTMLButtonElement> &
   Stitches.VariantProps<typeof S.Button>
 
-export function IconButton({ icon, type, ...props }: Props) {
+const Wrapper = ({
+  tooltip,
+  children,
+}: WithChildren<{ tooltip?: React.ReactNode }>) => {
+  if (!tooltip) return <>{children}</>
+  return <Tooltip tooltip={tooltip}>{children}</Tooltip>
+}
+
+export function IconButton({ icon, tooltip, type, ...props }: Props) {
   return (
-    <S.Button className="icon-button" data-icon-button type={type} {...props}>
-      {icon}
-    </S.Button>
+    <Wrapper tooltip={tooltip}>
+      <S.Button className="icon-button" data-icon-button type={type} {...props}>
+        {icon}
+      </S.Button>
+    </Wrapper>
   )
 }
 
