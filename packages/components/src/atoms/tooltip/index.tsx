cf6cd778cf89c8e18139847d8b8672c92dde9cb6
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-11-02 21:35:44 -0300

    :sparkles: feature(components/atoms/tooltip): add some useful props

--- packages/components/src/atoms/tooltip/index.tsx
+++ packages/components/src/atoms/tooltip/index.tsx
@@ -4,13 +4,29 @@ import * as S from './style'
 
 type Props = {
   tooltip: React.ReactNode
-}
+} & React.ComponentProps<typeof RTooltip.Root> &
+  React.ComponentProps<typeof RTooltip.Content>
 
-export function Tooltip({ children, tooltip }: WithChildren<Props>) {
+export function Tooltip({
+  children,
+  tooltip,
+  open,
+  defaultOpen,
+  onOpenChange,
+  delayDuration,
+  skipDelayDuration,
+  ...props
+}: WithChildren<Props>) {
   return (
-    <RTooltip.Root>
+    <RTooltip.Root
+      open={open}
+      defaultOpen={defaultOpen}
+      onOpenChange={onOpenChange}
+      delayDuration={delayDuration}
+      skipDelayDuration={skipDelayDuration}
+    >
       <RTooltip.Trigger asChild>{children}</RTooltip.Trigger>
-      <S.Content>
+      <S.Content side="top" align="center" {...props}>
         <S.Arrow />
         {tooltip}
       </S.Content>
