d45818c05dc3e8bbedeafe9926b0481cc93ea829
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-11-05 18:07:05 -0300

    :sparkles: feature(dashboard): add dashboard package

---
+++ packages/dashboard/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => (
+  <div>
+    <h1>Hello, world!</h1>
+    <p>Programmed to work and not to feel.</p>
+  </div>
+)
+
+export default Home
