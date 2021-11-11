ff4e7700f4aa8d24256cba7215e0514ca51d57e4
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2021-11-11 15:34:21 -0300

    :sparkles: feature(ui/atoms/inputs): add error state to Input and Password

--- packages/ui/src/atoms/inputs/input.tsx
+++ packages/ui/src/atoms/inputs/input.tsx
@@ -8,16 +8,29 @@ export type Props = {
   label: string
   helperText?: string
   icon?: React.ReactNode
+  error?: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export function Input({ label, icon, name, helperText, ...props }: Props) {
-  return (
-    <S.Wrapper>
+export const Input = React.forwardRef<HTMLInputElement, Props>(
+  ({ label, icon, name, helperText, error, ...props }, ref) => (
+    <S.Wrapper data-invalid={error ? true : undefined}>
       <S.Label htmlFor={name}>{label}</S.Label>
       <S.InputWrapper>
         {icon}
-        <S.Input name={name} {...props} />
+        <S.Input aria-invalid={!!error} ref={ref} name={name} {...props} />
       </S.InputWrapper>
+      {error && (
+        <Text
+          as="small"
+          size="sm"
+          css={{
+            marginTop: '$2',
+            color: '$errorRed400',
+          }}
+        >
+          {error}
+        </Text>
+      )}
       {helperText && (
         <Text
           size="sm"
@@ -30,4 +43,4 @@ export function Input({ label, icon, name, helperText, ...props }: Props) {
       )}
     </S.Wrapper>
   )
-}
+)
--- packages/ui/src/atoms/inputs/password.tsx
+++ packages/ui/src/atoms/inputs/password.tsx
@@ -9,37 +9,59 @@ export type Props = {
   label: string
   icon?: React.ReactNode
   helperText?: string
+  error?: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export function Password({ label, icon, name, helperText, ...props }: Props) {
-  const [show, toggle] = useToggle()
-  return (
-    <S.Wrapper>
-      <S.Label htmlFor={name}>{label}</S.Label>
-      <S.InputWrapper>
-        {icon}
-        <S.Input name={name} {...props} type={show ? 'text' : 'password'} />
-        <IconButton
-          aria-label="Alternar visibilidade da senha"
-          role="switch"
-          aria-checked={show}
-          aria-describedby={name}
-          onClick={toggle}
-          color="ghost"
-          type="button"
-          icon={<Icon name={show ? 'eye-slashed' : 'eye'} />}
-        />
-      </S.InputWrapper>{' '}
-      {helperText && (
-        <Text
-          size="sm"
-          css={{
-            color: '$spaceDark400',
-          }}
-        >
-          {helperText}
-        </Text>
-      )}
-    </S.Wrapper>
-  )
-}
+export const Password = React.forwardRef<HTMLInputElement, Props>(
+  ({ label, icon, name, helperText, error, ...props }, ref) => {
+    const [show, toggle] = useToggle()
+    return (
+      <S.Wrapper data-invalid={error ? true : undefined}>
+        <S.Label htmlFor={name}>{label}</S.Label>
+        <S.InputWrapper>
+          {icon}
+          <S.Input
+            aria-invalid={!!error}
+            ref={ref}
+            name={name}
+            {...props}
+            type={show ? 'text' : 'password'}
+          />
+          <IconButton
+            aria-label="Alternar visibilidade da senha"
+            role="switch"
+            aria-checked={show}
+            aria-describedby={name}
+            onClick={toggle}
+            color="ghost"
+            type="button"
+            icon={<Icon name={show ? 'eye-slashed' : 'eye'} />}
+          />
+        </S.InputWrapper>
+        {error && (
+          <Text
+            as="small"
+            size="sm"
+            css={{
+              marginTop: '$2',
+              color: '$errorRed400',
+            }}
+          >
+            {error}
+          </Text>
+        )}
+
+        {helperText && (
+          <Text
+            size="sm"
+            css={{
+              color: '$spaceDark400',
+            }}
+          >
+            {helperText}
+          </Text>
+        )}
+      </S.Wrapper>
+    )
+  }
+)
