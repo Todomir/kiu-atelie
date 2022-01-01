c8708baeac27974991c0dee343f4cbc14cb37558
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2022-01-01 16:33:55 -0300

    :white_check_mark: test(ui/components/button): add tests for button component

---
+++ packages/ui/src/components/button/__tests__/button.spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import React from "react";
+
+import { Icon } from "../..";
+import { Button } from "..";
+
+describe("<Button />", () => {
+  test("should render", () => {
+    render(<Button>Button</Button>);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/button/i);
+  });
+  test("should execute onClick", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Button</Button>);
+    const button = screen.getByRole("button");
+    user.click(button);
+    expect(onClick).toHaveBeenCalled();
+  });
+  test("should render with icon", () => {
+    const { rerender } = render(
+      <Button rightIcon={<Icon name="trashcan" />}>Button</Button>
+    );
+
+    const rightIcon = screen.getByRole("img");
+    const button = screen.getByRole("button");
+
+    expect(rightIcon).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/button/i);
+    expect(button).toContainElement(rightIcon);
+
+    rerender(<Button leftIcon={<Icon name="trashcan" />}>Button</Button>);
+
+    const leftIcon = screen.getByRole("img");
+
+    expect(leftIcon).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/button/i);
+    expect(button).toContainElement(leftIcon);
+  });
+});
