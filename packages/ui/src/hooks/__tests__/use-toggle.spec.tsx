2a4eb16a9890b162d58004fb5a156deefa6a96bf
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2022-01-01 17:43:10 -0300

    :white_check_mark: test(ui/hooks/use-toggle): add tests for useToggle hook

---
+++ packages/ui/src/hooks/__tests__/use-toggle.spec.tsx
@@ -0,0 +1,36 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+
+import { useToggle } from "../use-toggle";
+
+describe("useToggle", () => {
+  test("should initialize as false", () => {
+    const { result } = renderHook(() => useToggle());
+    const [value] = result.current;
+    expect(value).toBe(false);
+  });
+
+  test("should change to opposite when toggled", () => {
+    const { result } = renderHook(() => useToggle());
+    const [, toggle] = result.current;
+    act(() => {
+      toggle();
+    });
+    const [value] = result.current;
+    expect(value).toBe(true);
+  });
+
+  test("should initialize with provided value", () => {
+    const { result } = renderHook(
+      ({ initialValue }) => useToggle(initialValue),
+      {
+        initialProps: { initialValue: true },
+      }
+    );
+    expect(result.current[0]).toBe(true);
+
+    act(() => {
+      result.current[1]();
+    });
+    expect(result.current[0]).toBe(false);
+  });
+});
